cb51c36ffb45318ab04c0ddb3dbbf974c2217cce
Fadi K <fadi@Kelzia.com>
2025-04-06 06:36:13 +0400

    Add JavaScript version of Next.js config

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/skyble.ai.website.draftv1',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig; 
